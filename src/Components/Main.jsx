4fde2016721cd4592046f84a5202fe02c2259210
Alexey Shumilin <shumil.alexey@gmail.com>
2021-05-22 15:32:15 +0300

    components Video, VideoHeader
    added components Video and VideoHeader

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Article from './Article';
 import Place from './Place';
+import First from './First';
+import Video from './Video';
+import VideoHeader from './Video_Header';
 
 function Main() {
   return (
     <main className="content page__content">
-      <Place />
-      <Article />
+      <First/>
+      <Place/>
+      <Article/>
+      <Video/>
+      <VideoHeader/>
     </main>
   );
 }
---
+++ src/Components/Video.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function Video() {
+  return (
+    <section className="four-columns">
+      <article className="block-video">
+        <iframe className="block-video__iframe"/>
+        <h2 className="block-video__title">Жутко громко и запредельно близко</h2>
+        <p className="block-video__caption">Василий Сигарев, Борисов-Мусотов (Россия), 2009 год</p>
+        <a href="/#" className="block-video__category">смотреть трейлер</a>
+        <div className="block-video__caption block-video__caption_type_overflow">
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+        </div>
+      </article>
+      <article className="block-video">
+        <iframe className="block-video__iframe"/>
+        <h2 className="block-video__title">Жизнь Кабачка</h2>
+        <p className="block-video__caption">Клод Баррас, мультфильм, Швейцария, Франция, 2016</p>
+        <a href="/#" className="block-video__category">смотреть трейлер</a>
+        <div className="block-video__caption block-video__caption_type_overflow">
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+        </div>
+      </article>
+      <article className="block-video">
+        <iframe className="block-video__iframe"/>
+        <h2 className="block-video__title">Волчок</h2>
+        <p className="block-video__caption">Василий Сигарев, Россия, 2009 год</p>
+        <a href="/#" className="block-video__category">смотреть трейлер</a>
+        <div className="block-video__caption block-video__caption_type_overflow">
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+        </div>
+      </article>
+      <article className="block-video">
+        <iframe className="block-video__iframe"/>
+        <h2 className="block-video__title">Жутко громко и запредельно близко</h2>
+        <p className="block-video__caption">Василий Сигарев, Борисов-Мусотов (Россия), 2009 год</p>
+        <a href="/#" className="block-video__category">смотреть трейлер</a>
+        <div className="block-video__caption block-video__caption_type_overflow">
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+          <a href="/#"
+             className="block-video__category block-video__category_type_overflow-video">рубрика</a>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default Video;
